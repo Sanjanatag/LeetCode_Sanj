032fbae539985bab0446315ec205b9421e83cf6a
Sanjana Tagare <162710321+Sanjanatag@users.noreply.github.com>
2025-09-03 23:23:23 -0400

    Time: 1 ms (34.71%), Space: 41.2 MB (30.93%) - LeetHub

---
+++ 3830-find-closest-person/3830-find-closest-person.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        int xz= Math.abs(x-z);
+        int yz= Math.abs(y-z);
+        if(xz == yz)
+        return 0;
+        if(xz > yz)
+        return 2;
+        if(xz< yz)
+        return 1;
+
+        return 0;
+    }
+}
