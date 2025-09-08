a6b143b677435fa26ab5a3592b11baeccbbbc320
Sanjana Tagare <162710321+Sanjanatag@users.noreply.github.com>
2025-09-08 14:30:30 -0400

    Time: 1 ms (77.58%), Space: 46.3 MB (19.81%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int low= prices[0];
+        int profit=0;
+        for(int i=1; i < prices.length;i++){
+            if(prices[i]>low){
+                profit+= prices[i]-low;
+            } 
+                low= prices[i];
+            
+        }
+        return profit;
+    }
+}
